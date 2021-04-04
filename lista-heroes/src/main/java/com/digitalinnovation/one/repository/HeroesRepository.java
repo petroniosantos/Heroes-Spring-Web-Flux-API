b277d89a33889a8db388ec033e6c0fab79e00c9a
Petrônio Santos <petroniosoliveira@gmail.com>
2021-04-04 16:52:35 -0300

    Initial commit

---
+++ lista-heroes/src/main/java/com/digitalinnovation/one/repository/HeroesRepository.java
@@ -0,0 +1,9 @@
+package com.digitalinnovation.one.repository;
+
+import com.digitalinnovation.one.document.Heroes;
+import org.socialsignin.spring.data.dynamodb.repository.EnableScan;
+import org.springframework.data.repository.CrudRepository;
+
+@EnableScan
+public interface HeroesRepository extends CrudRepository<Heroes, String>{
+}
